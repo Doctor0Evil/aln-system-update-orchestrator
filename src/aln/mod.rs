39dc2c552f423841fb0a196fa8d507481f028328
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-02-06 18:33:05 -0700

    Update mod.rs

--- src/aln/mod.rs
+++ src/aln/mod.rs
@@ -1,4 +1,13 @@
+pub mod ast;
+pub mod lexer;
 pub mod parser;
+pub mod model;
 
 pub use parser::{AlnUpdatePlan, LoadAlnError};
-
+pub use model::{
+    AlnAction,
+    AlnComponentConfig,
+    AlnInteropConfig,
+    AlnRenderConfig,
+    AlnRegoExecConfig,
+};
